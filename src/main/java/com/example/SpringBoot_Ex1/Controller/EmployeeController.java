ac9b041b51b8ab9e8000d231d106cefcb86a038e
Bala <balachandar9018@gmail.com>
2021-04-13 21:04:04 +0530

    first local commit

---
+++ src/main/java/com/example/SpringBoot_Ex1/Controller/EmployeeController.java
@@ -0,0 +1,16 @@
+package com.example.SpringBoot_Ex1.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+
+@Controller
+public class EmployeeController {
+	@GetMapping("/api")
+	@ResponseBody
+	public String getemployee()	
+	{
+		return "Hello Bala your Spring boot appplicat0ion Started";
+	}
+}
---
+++ src/test/java/com/example/SpringBoot_Ex1/SpringBootEx1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SpringBoot_Ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEx1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
